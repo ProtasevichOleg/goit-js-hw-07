cd6da3c5489652c7e483cab10a245691a2ac9dca
Oleh Protasevych <protasevicholeg@gmail.com>
2023-03-06 14:17:14 +0200

    Do task-02 Simple Lightbox

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,31 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+
+function createGalleryMarkup(galleryItems) {
+  return galleryItems
+    .map(
+      ({ preview, original, description }) =>
+        `<div class="gallery__item">
+          <a class="gallery__link" href="${original}">
+            <img
+              class="gallery__image"
+              src="${preview}"
+              alt="${description}"
+            />
+          </a>
+        </div>`,
+    )
+    .join('');
+}
+
+galleryContainer.insertAdjacentHTML(
+  'beforeend',
+  createGalleryMarkup(galleryItems),
+);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionsData: 'alt',
+  captionDelay: 250,
+});
